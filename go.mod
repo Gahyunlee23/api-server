533473593737f24a75b0be97e8a95f5140ac0657
gahyun <gahyunlee1119@gmail.com>
2024-09-09 11:00:59 -0400

    add some more func and type for testing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module api-server
 
 go 1.13
+
+require github.com/rs/cors v1.11.1
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/rs/cors"
 	"log"
 	"net/http"
 )
@@ -12,19 +13,31 @@ type User struct {
 	Email string `json:"email"`
 }
 
-// HelloHandler는 간단한 텍스트 응답을 반환합니다.
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+type Product struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Code string `json:"code"`
 }
 
-// GetUserHandler는 User 객체를 JSON으로 반환합니다.
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func GetProductHandler(w http.ResponseWriter, r *http.Request) {
+	products := []Product{
+		{Id: 70, Name: "\tStandard Business card", Code: "pp_business_card"},
+		{Id: 40, Name: "\tLabels (Cut Sheet)", Code: "pp_labelscutsheet_np"},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
+
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	user := User{Name: "Stella", Email: "stella@example.com"}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
 }
 
-// PostUserHandler는 POST 요청으로 받은 JSON 데이터를 처리합니다.
 func PostUserHandler(w http.ResponseWriter, r *http.Request) {
 	var user User
 	json.NewDecoder(r.Body).Decode(&user)
@@ -32,12 +45,18 @@ func PostUserHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", HelloHandler)
-	http.HandleFunc("/user", GetUserHandler)    // GET 요청을 처리
-	http.HandleFunc("/create", PostUserHandler) // POST 요청을 처리
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HelloHandler)
+	mux.HandleFunc("/user", GetUserHandler)
+	mux.HandleFunc("/create", PostUserHandler)
+	mux.HandleFunc("/product", GetProductHandler)
+
+	// CORS 허용 설정
+	handler := cors.Default().Handler(mux)
 
 	fmt.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
 }
