580c2f28f3a4a2deaab35ed8af1d14143cfcd6c7
gahyun <gahyunlee1119@gmail.com>
2024-09-12 17:04:32 -0400

    add some more response for the pagination test

--- internal/controllers/product.go
+++ internal/controllers/product.go
@@ -8,8 +8,13 @@ import (
 
 func GetProductHandler(w http.ResponseWriter, r *http.Request) {
 	products := []models.Product{
-		{Id: 70, Name: "\tStandard Business card", Code: "pp_business_card", Type: "upload_fullpage_configuration"},
-		{Id: 40, Name: "\tLabels (Cut Sheet)", Code: "pp_labelscutsheet_np", Type: "upload_personalization_fullpage_configuration"},
+		{Id: 70, Name: "\tStandard Business card", Code: "BRCHRSMLNG1", Type: "upload_fullpage_configuration"},
+		{Id: 40, Name: "\tLabels (Cut Sheet)", Code: "PSTRCDSMLNG4", Type: "\tupload_fullpage_configuration"},
+		{Id: 26, Name: "\tPostcards with Direct Mail by Canada Post", Code: "PSTRCDSMLNG3", Type: "\tupload_fullpage_configuration"},
+		{Id: 27, Name: "\t4D Lenticular Cards", Code: "4DLCARDS5", Type: "upload_personalization_fullpage_configuration"},
+		{Id: 28, Name: "\t\tBookmarks", Code: "BKMRKS6", Type: "\tupload_fullpage_configuration"},
+		{Id: 29, Name: "\tBottle Neck Hang Tags", Code: "BTTLNECKHT7", Type: "\tupload_fullpage_configuration"},
+		{Id: 30, Name: "\tBrochures", Code: "BRCHR8", Type: "\tupload_fullpage_configuration"},
 	}
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(products)
